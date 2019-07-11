acc658c2878a48d16184c0e21c5167b313ab166d
unknown <tcutlip08@gmail.com>
2019-07-11 19:13:09 -0400

    set up on click/ready functions

--- java.js
+++ java.js
@@ -1,1 +1,17 @@
-console.log("Hey");
+var config = {
+    apiKey: "AIzaSyAftdjYKnYfRfR_Z7jrQE3k6EPKDubuiOg",
+    authDomain: "groupstuff-3f245.firebaseapp.com",
+    databaseURL: "https://groupstuff-3f245.firebaseio.com",
+    projectId: "groupstuff-3f245",
+    storageBucket: "",
+    messagingSenderId: "956410588944",
+    appId: "1:956410588944:web:5b1ec220f10c0653"
+};
+
+$(document).ready(function () {
+    console.log("ready!");
+});
+
+$(document).on("click", "#submit", function () {
+
+});
