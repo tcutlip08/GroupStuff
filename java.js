9cbb97b3eb78fcd4d42050bce52b8c85d34bc4a1
unknown <tcutlip08@gmail.com>
2019-07-11 20:16:12 -0400

    displaying database items to screen

--- java.js
+++ java.js
@@ -10,29 +10,66 @@ var config = {
 
 firebase.initializeApp(config);
 
+var database = firebase.database();
+
 $(document).ready(function () {
     console.log("ready!");
 });
 
+var name = "";
+var role = "";
+var monthlyRate = 0;
+var date = "";
+
 $(document).on("click", "#submit", function () {
-    console.log("Hey")
+    event.preventDefault();
+    name = $("#eName").val().trim();
+    role = $("#eRole").val().trim();
+    monthlyRate = $("#eMR").val().trim();
+    date = $("#eDt").val().trim();
+
+    console.log(name);
+    console.log(role);
+    console.log(monthlyRate);
+    console.log(date);
+
+    database.ref().push({
+        name: name,
+        role: role,
+        monthlyRate: monthlyRate,
+        date: date
+    });
 });
 
-// database.ref().on("child_added", function(snapshot) {
+database.ref().on("child_added", function(snap) {
 
-//     if (snapshot.child("highBidder").exists() && snapshot.child("highPrice").exists()) {
-//       highBidder = snapshot.val().highBidder;
-//       highPrice = parseInt(snapshot.val().highPrice);
-//     }
-  
-//     console.log(highBidder);
-//     console.log(highPrice);
-//     $("#highest-bidder").text(highBidder);
-//     $("#highest-price").text(highPrice);
-  
-//   }, function(errorObject) {
-//     console.log("The read failed: " + errorObject.code);
-//   });
+    // if (snapshot.child("name").exists() && snapshot.child("highPrice").exists()) {
+    //   highBidder = snapshot.val().highBidder;
+    //   highPrice = parseInt(snapshot.val().highPrice);
+    // }
+
+    console.log(snap.val());
+
+    var nameDiv = $("<div>");
+    nameDiv.addClass("name").attr("data-name", snap.val().name).html(snap.val().name);
+
+    var roleDiv = $("<div>");
+    roleDiv.addClass("role").attr("data-role", snap.val().role).html(snap.val().role);
+    
+    var monthlyRateDiv = $("<div>");
+    monthlyRateDiv.addClass("monthlyRate").attr("data-monthlyRate", snap.val().monthlyRate).html(snap.val().monthlyRate);
+
+    var dateDiv = $("<div>");
+    dateDiv.addClass("name").attr("data-date", snap.val().date).html(snap.val().date);
+    
+    $("#displayName").prepend(nameDiv);
+    $("#displayRole").prepend(roleDiv);
+    $("#displayMonthlyRate").prepend(monthlyRateDiv);
+    $("#displayDate").prepend(dateDiv);
+
+  }, function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
 
 // database.ref().push({
 //     highBidder: bidderName,
